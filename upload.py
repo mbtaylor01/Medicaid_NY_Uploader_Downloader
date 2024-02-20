700b1d676b1770e4c1eaa5fc1912e8b2066e684a
mbtaylor01 <marktaylor1@protonmail.com>
2024-02-19 21:18:57 -0800

    Add logic to upload.py to select Professional Claims.x12 from the Batch Type dropdown box

--- upload.py
+++ upload.py
@@ -1,5 +1,6 @@
 import driver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from settings import SETTINGS as S
 
 
@@ -10,5 +11,10 @@ def upload(chrome_driver):
     send_button_id = "[id$='_ibSendBatch']"
     send_button = driver.clickable_element(chrome_driver, By.CSS_SELECTOR, send_button_id)
     send_button.click()
+
+    #  find the dropdown box specifying the type of file to upload and select it
+    batch_type_dropdown_id = "[id*='_ContentPlaceHolder1_ddlBatchType']"
+    batch_type_dropdown = driver.visible_element(chrome_driver, By.CSS_SELECTOR, batch_type_dropdown_id)
+    Select(batch_type_dropdown).select_by_value("Professional Claims.x12")
 
     
