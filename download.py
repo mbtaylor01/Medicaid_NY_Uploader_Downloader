5a65b3a4d6bfeeb4f357d20ec1e13072f472ac15
mbtaylor01 <marktaylor1@protonmail.com>
2024-02-21 21:13:34 -0800

    Add logic to download.py to get the contents of last_download.txt with the last_download function from download_functions.py

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import driver
+import download_functions as dfs
 from selenium.webdriver.common.by import By
 from settings import SETTINGS as S
 
@@ -10,3 +11,6 @@ def download(chrome_driver):
     inbox_button_id = "[id$='_ibInbox']"
     inbox_button = driver.clickable_element(chrome_driver, By.CSS_SELECTOR, inbox_button_id)
     inbox_button.click()
+
+    #  reads last_download.txt for the name of the last downloaded file
+    last_download = dfs.last_download()
