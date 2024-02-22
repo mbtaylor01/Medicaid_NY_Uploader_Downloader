c7d156a7d7416f9bebd62975733917240b82e56a
mbtaylor01 <marktaylor1@protonmail.com>
2024-02-21 21:50:04 -0800

    Add logic to download.py to click only the new download links in the list of files

--- download.py
+++ download.py
@@ -14,3 +14,19 @@ def download(chrome_driver):
 
     #  reads last_download.txt for the name of the last downloaded file
     last_download = dfs.last_download()
+
+    files_found_to_download = []
+
+    #  find each link in the file list from top to bottom (100 is arbitrary number)
+    for i in range(100):
+        link_container_id = f"[id$='_ContentPlaceHolder1_rgdProduction_ctl00__{i}']"
+        link_container = driver.visible_element(chrome_driver, By.CSS_SELECTOR, link_container_id)
+        link = link_container.find_element(By.TAG_NAME, "a")
+
+        #  if the file doesn't match the last downloaded file, record the name and click the link
+        #  if it does match, you've reached the end of new files to download
+        if link.text != last_download:
+            files_found_to_download.append(link.text)
+            link.click()
+        else:
+            break
