ac0d7698e3abd54dc88e022d36a86b29c02b44e4
mbtaylor01 <marktaylor1@protonmail.com>
2024-02-26 21:05:36 -0800

    Move the try:except inside the main function so that chrome_driver.quit can be called in the except block

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ import driver
 import login
 import upload
 import download
+from emails import send_email
+from traceback import format_exc
 from settings import SETTINGS as S
 
 
 def main():
-    #  create driver
-    chrome_driver = driver.create_driver()
+    try:
+        #  create driver
+        chrome_driver = driver.create_driver()
 
-    login.login(chrome_driver)
+        login.login(chrome_driver)
 
-    # upload files
-    upload.upload(chrome_driver)
+        # upload files
+        upload.upload(chrome_driver)
 
-    #  download files
-    download.download(chrome_driver)
 
-    # quit driver
-    if eval(S['browser_is_headless']):
+        #  download files
+        download.download(chrome_driver)
+
+        # quit driver
+        if eval(S['browser_is_headless']):
+            chrome_driver.quit()
+
+    except Exception as exception:
+        send_email(S["main_fail_subject"], f"{exception}\r\r{format_exc()}")
+
         chrome_driver.quit()
 
+
 main()
+
+
